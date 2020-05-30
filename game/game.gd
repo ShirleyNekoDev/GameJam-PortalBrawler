cf632290a928e4063df31086636e1d74e0db8814
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2020-05-30 11:57:43 +0200

    Initial commit

---
+++ game/game.gd
@@ -0,0 +1,81 @@
+extends Node2D
+
+var port = 8877
+var ip = 'localhost'
+var max_players = 200
+
+func _ready():
+	var peer = NetworkedMultiplayerENet.new()
+	var is_client = "--client" in OS.get_cmdline_args()
+	var is_dedicated = "--dedicated" in OS.get_cmdline_args()
+	
+	if is_client:
+		peer.create_client(ip, port)
+		get_tree().connect("server_disconnected", self, "client_note_disconnected")
+	else:
+		print("Listening for connections on " + String(port) + " ...")
+		peer.create_server(port, max_players)
+		get_tree().connect("network_peer_connected", self, "server_player_connected")
+		get_tree().connect("network_peer_disconnected", self, "server_player_disconnected")
+		if not is_dedicated:
+			register_player(1, null, {})
+	
+	get_tree().set_network_peer(peer)
+
+func client_note_disconnected():
+	print("Server disconnected from player, exiting ...")
+	get_tree().quit()
+
+func server_player_connected(player_id: int):
+	if player_id != 1:
+		print("Connected ", player_id)
+		# get our new player informed about all the old players and objects
+		for old_player in get_tree().get_nodes_in_group("players"):
+			rpc_id(player_id, "register_player", old_player.id, old_player.position, old_player.get_sync_state())
+		for node in get_tree().get_nodes_in_group("synced"):
+			rpc_id(player_id, "spawn_object", node.name, node.filename, node.position, node.get_node("sync").get_sync_state())
+		
+		# inform all our players about the new player
+		var new_player = register_player(player_id, null, {})
+		rpc("register_player", player_id, new_player.position, new_player.get_sync_state())
+
+func server_player_disconnected(player_id: int):
+	print("Disconnected ", player_id)
+	rpc("unregister_player", player_id)
+
+remote func spawn_object(name: String, filename: String, position: Vector2, state: Dictionary):
+	# either create the object or just find the existing one
+	var object: Node2D = get_node_or_null(name)
+	if not object:
+		object = load(filename).instance()
+		object.name = name
+		add_child(object)
+	
+	# rigid bodys need to be our syncable_rigid_body because you can't set the
+	# position or any other physics property outside of its own _integrate_forces
+	if object is RigidBody2D:
+		object.use_update(position, state)
+	else:
+		object.position = position
+		for property in state:
+			object.set(property, state[property])
+	
+	return object
+
+remote func register_player(player_id: int, position, state: Dictionary):
+	var player: Node2D = preload("res://player/player.tscn").instance()
+	player.id = player_id
+	player.set_network_master(player.id)
+	player.name = String(player.id)
+	player.add_to_group("players")
+	
+	add_child(player)
+	
+	if position:
+		player.position = position
+	for property in state:
+		player.set(property, state[property])
+	return player
+
+remotesync func unregister_player(player_id: int):
+	remove_child(get_node(String(player_id)))
---
+++ player/player.gd
@@ -0,0 +1,63 @@
+extends KinematicBody2D
+class_name Player
+
+var id
+var color: Color setget set_color
+const speed = 200
+
+func _ready():
+	rset_config("position", MultiplayerAPI.RPC_MODE_REMOTESYNC)
+	set_process(true)
+	randomize()
+	position = Vector2(rand_range(0, get_viewport_rect().size.x), rand_range(0, get_viewport_rect().size.y))
+	
+	# pick our color, even though this will be called on all clients, everyone
+	# else's random picks will be overriden by the first sync_state from the master
+	set_color(Color.from_hsv(randf(), 1, 1))
+
+func get_sync_state():
+	# place all synced properties in here
+	var properties = ['color']
+	
+	var state = {}
+	for p in properties:
+		state[p] = get(p)
+	return state
+
+func _process(dt):
+	if is_network_master():
+		if Input.is_action_pressed("ui_up"):
+			rset("position", position + Vector2(0, -speed * dt))
+		if Input.is_action_pressed("ui_down"):
+			rset("position", position + Vector2(0, speed * dt))
+		if Input.is_action_pressed("ui_left"):
+			rset("position", position + Vector2(-speed * dt, 0))
+		if Input.is_action_pressed("ui_right"):
+			rset("position", position + Vector2(speed * dt, 0))
+		if Input.is_action_just_pressed("ui_accept"):
+			rpc("spawn_box", position)
+		if Input.is_mouse_button_pressed(BUTTON_LEFT):
+			var direction = -(position - get_viewport().get_mouse_position()).normalized()
+			rpc("spawn_projectile", position, direction, Uuid.v4())
+
+func set_color(_color: Color):
+	color = _color
+	$sprite.modulate = color
+
+remotesync func spawn_projectile(position, direction, name):
+	var projectile = preload("res://examples/physics_projectile/physics_projectile.tscn").instance()
+	projectile.set_network_master(1)
+	projectile.name = name
+	projectile.position = position
+	projectile.direction = direction
+	projectile.owned_by = self
+	get_parent().add_child(projectile)
+	return projectile
+
+remotesync func spawn_box(position):
+	var box = preload("res://examples/block/block.tscn").instance()
+	box.position = position
+	get_parent().add_child(box)
+
+remotesync func kill():
+	hide()
